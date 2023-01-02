79f2fbb7e8a5fc0032df489a767f71d489bb1bc6
krkopl <krko1973@gmail.com>
2023-01-02 20:16:34 +0100

    komentarze i uklad

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
-const commentRoutes = require("./routes/commentRoutes")
+const commentRoutes = require("./routes/commentRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -20,19 +20,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/comments", commentRoutes);
 
-
 //app.get("/api/notes", cors(), (req, res) => {
-  //res.json(notes);
+//res.json(notes);
 //});
 
 app.use(notFound);
 app.use(errorHandler);
+
 // Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
