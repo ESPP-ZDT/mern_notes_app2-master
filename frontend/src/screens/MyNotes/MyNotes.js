c33c6ee02c1cfe8031a42805a588acfe4d11d379
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-08 01:00:26 +0100

    implemented search in the landing page.

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -56,7 +56,9 @@ const MyNotes = ({search}) => {
         {loading && <Loading />}
         {notes?.reverse().filter(
           filteredNote=>(
-            filteredNote.title.toLowerCase().includes(search.toLowerCase())
+            filteredNote.title.toLowerCase().includes(search.toLowerCase()),
+            filteredNote.content.toLowerCase().includes(search.toLowerCase())
+            
           )
         ).map((note) => (
           <Card key={note._id}>
